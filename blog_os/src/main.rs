fad2f2638d8abc9ac754de77d5641991150d2bab
Aneel Badesha <aneel_badesha@sfu.ca>
2025-09-19 00:11:16 -0700

    Updated for bootloader

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -5,7 +5,8 @@
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+static HELLO: &[u8] = b"Hello World!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -13,7 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
